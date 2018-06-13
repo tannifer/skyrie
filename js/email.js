10f38bdc14b46c1166af3959bfb742e105783b38
Kevin Thomson <kevin.thomson1@gmail.com>
2018-06-13 19:14:13 +0100

    Added sendEmail.php for booking form. Updated Wedding Packages to horixontal lines.

--- js/email.js
+++ js/email.js
@@ -23,5 +23,5 @@ function sendMail() {
      + 'Design your own: ' + own + '%0D%0A'
      + 'Venue Adress: ' + vaddress + '%0D%0A'
      + 'Aditional information: ' + additionalInfo + '%0D%0A' ;
-    window.location.href = 'mailto:shorty.les@gmail.com?subject=The subject - booking request for ' + name + ' (' + email + ')' + '&body=' + message;
+    location.href = 'mailto:shorty.les@gmail.com?subject=The subject - Skyrie booking request for ' + name + ' (' + email + ')' + '&body=' + message;
 };
